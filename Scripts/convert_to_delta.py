2e6923bf7754a2210957e8d79edb92a3ddbecb80
HauMoon2606 <conghau266255@gmail.com>
2024-10-21 22:37:19 +0700

    comple folder scripts

---
+++ Scripts/convert_to_delta.py
@@ -0,0 +1,77 @@
+import sys
+import os
+import warnings
+import traceback
+import logging
+import time
+from minio import Minio
+from pyspark  import SparkConf,SparkContext
+
+utils_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils'))
+sys.path.append(utils_path)
+
+from helper import load_cfg
+from minio_utils import MinIOClient
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+warnings.filterwarnings('ignore')
+
+cfg = load_cfg("../Config/datalake.yaml")
+datalake_cfg = cfg["datalake"] 
+
+MINIO_ENDPOINT = datalake_cfg["endpoint"]
+MINIO_ACCESS_KEY = datalake_cfg["access_key"]
+MINIO_SCRET_KEY = datalake_cfg["secret_key"]
+bucket_name_2 = datalake_cfg["bucket_name_2"]
+bucket_name_3 = datalake_cfg["bucket_name_3"]
+
+def delta_convert(endpoint_url, access_key, secret_key):
+    from pyspark.sql import SparkSession
+    from delta.pip_utils import configure_spark_with_delta_pip
+
+    jars = "jars/hadoop-aws-3.3.4.jar,jars/aws-java-sdk-bundle-1.12.262.jar"
+
+    builder = SparkSession.builder \
+                    .appName("Converting to Delta Lake") \
+                    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+                    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+                    .config("spark.hadoop.fs.s3a.access.key", access_key) \
+                    .config("spark.hadoop.fs.s3a.secret.key", secret_key) \
+                    .config("spark.hadoop.fs.s3a.endpoint", endpoint_url) \
+                    .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+                    .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+                    .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
+                    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+                    .config("spark.hadoop.fs.s3a.connection.ssl.enabled", "false") \
+                    .config("spark.jars", jars)
+        
+    spark = configure_spark_with_delta_pip(builder, extra_packages=["org.apache.hadoop:hadoop-aws:3.3.4"]).getOrCreate()
+    logging.info('Spark session successfully created!')
+
+    client = MinIOClient(
+        endpoint = endpoint_url,
+        access_key = access_key,
+        secret_key = secret_key
+    )
+    client.create_bucket(bucket_name_3)
+    for file in client.list_parquet_files(bucket_name_2, prefix = datalake_cfg["folder_name"]):
+        path_read = f"s3a://{bucket_name_2}/" +file
+        logging.info(f"Reading parquet file: {file}")
+
+        df = spark.read.parquet(path_read)
+
+        path_read = f"s3a://{bucket_name_3}/" +file
+        logging.info(f"Saving delta file: {file}")
+
+        df_delta = df.write \
+                    .format("delta") \
+                    .mode("overwrite") \
+                    .save(f"s3a://{bucket_name_3}/{datalake_cfg["folder_name"]}")
+        logging.info("="*50 +"Complete" +"="*50)
+
+if __name__ == "__main__":
+    delta_convert(MINIO_ENDPOINT,MINIO_ACCESS_KEY,MINIO_SCRET_KEY)
+
+
+
+    
---
+++ Scripts/transform_data.py
@@ -0,0 +1,139 @@
+import pandas as pd
+from glob import glob
+import sys
+import os
+from minio import Minio
+import time
+import s3fs
+
+utils_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils'))
+sys.path.append(utils_path)
+
+from helper import load_cfg
+from minio_utils import MinIOClient
+
+cfg_file = "../Config/datalake.yaml"
+taxi_lookup_path = os.path.join(os.path.dirname(__file__),"Data","taxi_lookup.csv")
+years = ["2022"]
+
+def drop_column(df:pd.DataFrame, file):
+    if "store_and_fwd_flag" in df.columns:
+        df=df.drop(columns=["store_and_fwd_flag"])
+        print(f"Dropped column store_and_fwd_flag from file {file}")
+    else:
+        print(f"Column store_and_fwd_flag not found in file {file}")
+    return df
+
+def merge_taxi_zone(df:pd.DataFrame, file):
+    df_lookup = pd.read_csv(taxi_lookup_path)
+
+    def merge_and_rename(df:pd.DataFrame,location_id,lat_col,long_col):
+        df = df.merge(df_lookup, left_on=location_id, right_on="LocationID")
+        df = df.drop(columns=["LocationID","Borough","service_zone","zone"])
+        df = df.rename(columns={
+            "latitude":lat_col,
+            "longitude":long_col
+        })
+        return df
+    
+    if "pickup_latitude" not in df.columns:
+        df = merge_and_rename(df,"pulocationid","pickup_latitude","pickup_longitude")
+    if "dropoff_latitude" not in df.columns:
+        df = merge_and_rename(df,"dolocationid","dropoff_latitude","dropoff_longitude")
+
+    df = df.drop(columns= [col for col in df.columns if "Unname" in col],errors='ignore').dropna()
+    print(f"Merged data from file {file}")
+    return df
+
+def process(df:pd.DataFrame, file:str):
+    if file.startswith("green"):
+        df.rename(
+            columns={
+                "lpep_pickup_datetime":"pickup_datetime",
+                "lpep_dropoff_datetime":"dropoff_datetime",
+                "ehail_fee":"fee"
+            },
+            inplace= True
+        )
+        if "trip_type" in df.columns:
+            df.drop(columns=["trip_type"], inplace=True)   
+    elif file.startswith("yellow"):
+        df.rename(
+            columns={
+                "tpep_pickup_datetime":"pickup_datetime",
+                "tpep_dropoff_datetime":"dropoff_datetime",
+                "airport_fee": "fee"
+            },
+            inplace=True
+        )
+    if "payment_type" in df.columns:
+        df["payment_type"] = df["payment_type"].astype(int)
+    if "dolocationid" in df.columns:
+        df["dolocationid"] = df["dolocationid"].astype(int)
+    if "pulocationid" in df.columns:
+        df["pulocationid"] = df["pulocationid"].astype(int)
+    if "vendorid" in df.columns:
+        df["vendorid"] = df["vendorid"].astype(int)
+
+    if "fee" in df.columns:
+        df.drop(columns=["fee"], inplace=True)
+
+    df = df.dropna()
+    df = df.reindex(sorted(df.columns),axis=1)
+
+    print("Transformed data from file: "+file)
+    return df
+
+def transform_data(endpoint_url, access_key, secret_key):
+    cfg = load_cfg(cfg_file)
+    datalake_cfg = cfg["datalake"]
+    nyc_data_cfg = cfg["nyc_data"]
+
+    client = MinIOClient(
+        endpoint=endpoint_url,
+        access_key=access_key,
+        secret_key=secret_key
+    )
+    client.create_bucket(datalake_cfg['bucket_name_2'])
+
+    for year in years:
+        data_files_local = glob(os.path.join("..",nyc_data_cfg["folder_path"],year,"*.parquet"))
+        for file in data_files_local:
+            file_name = file.split("\\")[-1]
+            print(f"Reading parquet file {file_name}")
+            df = pd.read_parquet(file,engine="pyarrow")
+            df.columns = map(str.lower,df.columns)
+
+            df = drop_column(df, file_name)
+            df = merge_taxi_zone(df,file_name)
+            df = process(df, file_name)
+
+            path_in_s3 = f"s3://{datalake_cfg['bucket_name_2']}/{datalake_cfg['folder_name']}/" + file_name
+
+            storage_options = {
+                'key': access_key,
+                'secret': secret_key,
+                'client_kwargs': {
+                    'endpoint_url': f"http://{endpoint_url}"
+                }
+            }
+
+            df.to_parquet(path_in_s3,index=False,storage_options=storage_options, engine="pyarrow" )
+            print("Finished transforming data in file: "+path_in_s3)
+            print("="*100)
+
+if __name__ == "__main__":
+    cfg = load_cfg(cfg_file)
+    datalake_cfg = cfg["datalake"]
+
+    endpoint_url_local = datalake_cfg["endpoint"]
+    access_key_local = datalake_cfg["access_key"]
+    secret_key_local = datalake_cfg["secret_key"]
+
+    transform_data(endpoint_url_local,access_key_local,secret_key_local)
+
+
+
+
+    
+    
